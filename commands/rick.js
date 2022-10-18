705f7ebf80baa7fd44143c598dc25e56dbf56418
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-10-19 01:50:51 +0200

    Rick command now operational!

--- commands/rick.js
+++ commands/rick.js
@@ -1,21 +1,38 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const rick = createAudioResource('C:/Users/Joel/OneDrive/Escriptori/Weas/Sam Project/Sam/commands/rick.mp3');
-const player = createAudioPlayer();
+const { generateDependencyReport, getVoiceConnection, AudioPlayerStatus, entersState, joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnectionStatus } = require('@discordjs/voice');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rick')
 		.setDescription('Rick rol'),
 	async execute(interaction) {
-		console.log(interaction.channelId);
-		const connection = joinVoiceChannel({
-			channelId: '848112953723060244',
+
+		const voiceConnection = joinVoiceChannel({
+			channelId: interaction.member.voice.channelId,
 			guildId: interaction.channel.guildId,
 			adapterCreator: interaction.channel.guild.voiceAdapterCreator,
+			selfDeaf: false
 		});
+
+		const connection = getVoiceConnection(interaction.guildId);
+		const player = createAudioPlayer();
+		const resource = createAudioResource('./audio/rick.ogg');
+
+		try {
+
+			await entersState(voiceConnection, VoiceConnectionStatus.Ready, 5000);
+			
+			console.log("Connected");
+			
+		} catch (error) {
+			
+			console.log("Voice Connection not ready within 5s.", error);
+			
+			return null;
+			
+		}
 		
-		player.play(rick);
 		connection.subscribe(player);
+		player.play(resource);
 		
 	},
 };

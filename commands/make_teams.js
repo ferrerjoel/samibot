1a0d53ed2f65c3ff87467c4345ae86e00d88eb25
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-10-26 22:36:49 +0200

    Added a parameter to create VC on teams!

--- commands/make_teams.js
+++ commands/make_teams.js
@@ -5,15 +5,19 @@ module.exports = {
 		.setName('teams')
 		.setDescription('Make teams of your VC!')
 		.addIntegerOption(option => option.setName('teams')
-				.setDescription('Number of teams do you want to make')
-				.setRequired(true)
-				.setMinValue(2)
-				.setMaxValue(10)),
+			.setDescription('Number of teams do you want to make')
+			.setRequired(true)
+			.setMinValue(2)
+			.setMaxValue(10))
+		.addBooleanOption(option => option.setName('voice')
+			.setDescription('Do you want to make voice channels?')
+			.setRequired(false)),
 	async execute(interaction) {
 		let userVoiceChannel = interaction.member.voice.channelId;
 		let serverMembers = interaction.guild.members.cache.map(member => member);
 
 		let teamsToCreate = interaction.options.getInteger('teams');
+		let voice = interaction.options.getBoolean('voice');
 
 		serverMembers = serverMembers.filter(e => e.voice.channelId == userVoiceChannel);
 		let numberOfPlayers = serverMembers.length;
@@ -37,31 +41,38 @@ module.exports = {
 			
 		}
 
-		await interaction.reply({ content: printTeams(teams)});
-		console.log(printTeams(teams));
+		await interaction.reply({ content: await printTeams(teams, voice, interaction)});
+		printTeams(teams);
 		
-		// if (true){
-		// 	interaction.guild.channels.create({
-		// 		name: 'HOLA',
-		// 		type: 2, //GUILD_VOICE
-		// 		permissionOverwrites: [
-		// 		   {
-		// 			 id: interaction.user.id,
-		// 			 deny: [PermissionsBitField.Flags.ViewChannel],
-		// 		  },
-		// 		],
-		// 	  })
-		// }
 	},
 };
 
-function printTeams(teams){
+async function printTeams(teams, voice, interaction){
 	let messageTeams = "";
+	let channel;
 	for (let i = 0; i < teams.length; i++){
 		messageTeams += "**Team "+(i+1)+":**\n----------------\n";
+		if (voice){
+			channel = await createChannel(interaction, "Team " + (i+1))
+		} 
 		for (let j = 0; j < teams[i].length; j++){
 			messageTeams += teams[i][j][0].user.username + "\n";
+			if (voice) teams[i][j][0].voice.setChannel(channel);
 		}
 	}
 	return messageTeams;
+}
+
+async function createChannel(interaction, name){
+	channel = await interaction.guild.channels.create({
+		name: name,
+		type: 2, //GUILD_VOICE
+		permissionOverwrites: [
+			{
+				id: interaction.user.id,
+				deny: [PermissionsBitField.Flags.ViewChannel],
+			},
+		],
+	})
+	return channel;
 }

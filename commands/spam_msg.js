d7c487f72a6dbec8b169d54460d6f5b00f4f0613
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-12-03 01:30:13 +0100

    Added /spam_msg
    Sens a string to the current chat interaction x iterations

---
+++ commands/spam_msg.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { generateDependencyReport, getVoiceConnection, AudioPlayerStatus, entersState, joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnectionStatus } = require('@discordjs/voice');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('spam_msg')
+		.setDescription('Spams a msg')
+		.addIntegerOption(option => option.setName('iterations')
+			.setDescription('Number of iterations')
+			.setRequired(true)
+			.setMinValue(1)
+			.setMaxValue(100))
+		.addStringOption(option => option.setName('message')
+			.setDescription('Message to send')
+			.setRequired(true)),
+	async execute(interaction) {
+		await interaction.reply({content: 'Deploying...', ephemeral: true});
+		for (let i = 0; i < interaction.options.getInteger('iterations'); i++){
+			interaction.channel.send(interaction.options.getString('message'));
+		}
+		
+	},
+};

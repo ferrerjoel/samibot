0674e73f6909a07a3111f628dbd4c541f71714be
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-10-03 22:23:09 +0200

    Discord music bot now doesn't stop
    Update your dependencies before trying to fix things, people

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 const {
   getVoiceConnection,
   entersState,
@@ -6,23 +6,33 @@ const {
   createAudioPlayer,
   createAudioResource,
   VoiceConnectionStatus,
-  StreamType
-} = require('@discordjs/voice');
-const axios = require('axios');
+  StreamType,
+  AudioPlayerStatus,
+} = require("@discordjs/voice");
+const axios = require("axios");
+
+const queue = []; // Queue to hold the URLs to be played
+let isPlaying = false; // Flag to track if audio is currently playing
 const player = createAudioPlayer();
+
 module.exports = {
   player,
   data: new SlashCommandBuilder()
-    .setName('play')
-    .setDescription('Plays an online audio')
-    .addStringOption(option =>option.setName('url')
-        .setDescription('URL of the file to play')
-        .setRequired(true)),
+    .setName("play")
+    .setDescription("Plays an online audio")
+    .addStringOption((option) =>
+      option
+        .setName("url")
+        .setDescription("URL of the file to play")
+        .setRequired(true)
+    ),
   async execute(interaction) {
     const voiceChannelId = interaction.member.voice.channelId;
 
     if (!voiceChannelId) {
-      return interaction.reply('You need to be in a voice channel to use this command.');
+      return interaction.reply(
+        "You need to be in a voice channel to use this command."
+      );
     }
 
     const voiceConnection = joinVoiceChannel({
@@ -33,40 +43,67 @@ module.exports = {
     });
 
     const connection = getVoiceConnection(interaction.guildId);
-    const player = createAudioPlayer();
 
     try {
       await entersState(voiceConnection, VoiceConnectionStatus.Ready, 5000);
-      console.log(`Connected to voice channel ${voiceChannelId} on guild ${interaction.guildId}`);
+      console.log(
+        `Connected to voice channel ${voiceChannelId} on guild ${interaction.guildId}`
+      );
     } catch (error) {
       console.error("Voice Connection not ready within 5s.", error);
-      return interaction.reply({content: 'Unable to connect to the voice channel.', ephemeral: true});
+      return interaction.reply({
+        content: "Unable to connect to the voice channel.",
+        ephemeral: true,
+      });
     }
 
-    // Replace 'YOUR_MP3_URL' with the actual URL of the MP3 file
-    const mp3Url = interaction.options.getString('url');
+    const mp3Url = interaction.options.getString("url");
 
-    // Use axios to get the MP3 file from the URL
-    try {
-      const response = await axios.get(mp3Url, { responseType: 'stream' });
+    queue.push(mp3Url);
+
+    // If audio is not currently playing, start playing
+    if (!isPlaying) {
+      playQueue(connection);
+    }
+
+    interaction.reply({ content: `Added to the queue ;) Songs left: [${queue.length}]`, ephemeral: true });
+  },
+};
+
+function playQueue(connection) {
+
+  if (queue.length === 0) {
+    isPlaying = false;
+    return;
+  }
+
+  isPlaying = true;
+
+  const mp3Url = queue.shift(); 
+
+  // Use axios to get the MP3 file from the URL
+  axios
+    .get(mp3Url, { responseType: "stream" })
+    .then((response) => {
       const stream = response.data;
 
-      // Create an AudioResourceStream from the streamed data
       const resource = createAudioResource(stream, {
         inlineVolume: true,
-        inputType: StreamType.WebmOpus, // Use inputType 1 for Opus-based audio (typically used in voice channels)
+        //inputType: StreamType.WebmOpus,
       });
       resource.volume.setVolume(1);
 
-      // Play the audio
       player.play(resource);
-    } catch (error) {
+
+      connection.subscribe(player);
+
+      // Listen for the 'audioPlayerIdle' event to play the next item in the queue
+      player.once(AudioPlayerStatus.Idle, () => {
+        playQueue();
+      });
+    })
+    .catch((error) => {
       console.error("Error streaming and playing the MP3:", error);
-      return interaction.reply({ content: 'An error occurred while processing the audio.', ephemeral: true});
-    }
-
-    connection.subscribe(player);
-
-    interaction.reply({ content: 'Now playing the online audio.', ephemeral: true});
-  },
-};
+      playQueue();
+    });
+}

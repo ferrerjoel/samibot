5843e19a61393681363ada1e99252fbd545f553e
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-10-19 00:26:38 +0200

    Added splitter for daily text and launch.json
    Added splitter for daily text so it doesn't surpass the max length of the value of discord embeds and launch.json

--- commands/daily.js
+++ commands/daily.js
@@ -29,9 +29,9 @@ module.exports = {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     );
 
-    await page.goto(
-      `https://wol.jw.org/${interaction.options.getString("lang")}`
-    );
+    const url = `https://wol.jw.org/${interaction.options.getString("lang")}`
+
+    await page.goto(url);
 
     await page.waitForSelector(".tabContent.active .bodyTxt"); // Wait for the content section
 
@@ -40,17 +40,49 @@ module.exports = {
     const text = await page.$eval(".tabContent.active .themeScrp", (element) => element.textContent);
     const content = await page.$eval(".tabContent.active .bodyTxt .pGroup", (element) => element.textContent);
 
+    const splitPoint = findSplitPoint(content, content.length);
+    const firstHalf = content.slice(0, splitPoint);
+    const secondHalf = content.slice(splitPoint);
+        
     const embed = new EmbedBuilder()
       .setColor("496DA7")
       .setTitle(title)
-      .setURL("https://wol.jw.org/es/wol/h/r4/lp-s")
+      .setDescription(text)
+      .setURL(url)
       .setThumbnail(
         "https://img.asmedia.epimg.net/resizer/i6_OJPDtk1j53mPZjjVSZe0MIyM=/1472x1104/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KKF52TVGVNCKJFW6RGQRBR6OBE.png"
       )
-      .addFields({ name: text, value: content });
+      .addFields({ name: "\u200B", value: firstHalf })
+      .addFields({ name: "\u200B", value: secondHalf });
 
     await interaction.editReply({ content: "", embeds: [embed] });
 
     await browser.close();
+
+    function findSplitPoint(text, length) {
+
+      const sentenceEnders = [".", "!", "?"];
+    
+      let splitPoint = 0;
+      let prevEnder = -1;
+    
+      for (let i = 0; i < length; i++) {
+
+        if (sentenceEnders.includes(text[i])) {
+          prevEnder = i;
+        }
+    
+        if (i >= length / 2 && prevEnder >= 0) {
+          splitPoint = prevEnder + 1; 
+          break;
+        }
+      }
+    
+      if (splitPoint === 0) {
+        splitPoint = Math.floor(length / 2);
+      }
+    
+      return splitPoint;
+    }
   },
 };

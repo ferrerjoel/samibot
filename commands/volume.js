409ca447a6eefe8d81976cb70741e3a6f21e0f43
ferrerjoel <ferrerierajoel@gmail.com>
2023-11-24 23:01:01 +0100

    Added a volume method and it's dependences

--- commands/volume.js
+++ commands/volume.js
@@ -1,13 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { player } = require("./play")
+const {
+  getVoiceConnection,
+  entersState,
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  VoiceConnectionStatus,
+  StreamType,
+  AudioPlayerStatus,
+} = require("@discordjs/voice");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('volume')
-    .setDescription('Pauses or resumes the current audio'),
+    .setDescription('Pauses or resumes the current audio')
+    .addIntegerOption(option => option.setName('volume')
+      .setDescription('Number of volume')),
   async execute(interaction) {
 
+    const connection = getVoiceConnection(interaction.guildId);
 
+    const player = connection.state.subscription?.player;
+    if (player) {
+      const currentResource = player.state.resource;
+      const isPlaying = player.state.status === AudioPlayerStatus.Playing;
+      const volumeValue = interaction.options.getInteger('volume') || 1;
+      currentResource.volume.setVolume(volumeValue);
+    }
 
     interaction.reply({ content: 'Volume set!', ephemeral: true});
   },
